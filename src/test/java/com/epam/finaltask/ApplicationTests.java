7324bcb1761dcfd33f55ff07cfc327404776d722
Dmitriy.Koval <dimonkoval@gmail.com>
2025-03-30 23:10:43 +0300

    Added TestSecurityConfig

--- src/test/java/com/epam/finaltask/ApplicationTests.java
+++ src/test/java/com/epam/finaltask/ApplicationTests.java
@@ -1,14 +1,17 @@
 package com.epam.finaltask;
 
+import com.epam.finaltask.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestSecurityConfig.class)
 @ExtendWith(SpringExtension.class)
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @TestPropertySource(properties = "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect")
